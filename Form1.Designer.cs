3606ff8e3777e2f596a66d78da7375d00a7d9aaf
ChhoeurnKhuon <Khounchhoeun66@gmail.com>
2024-04-23 16:36:03 +0700

    5

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace MidtermExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtYearPublish = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code =";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Title =";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantity =";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(776, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 43);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Author =";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(742, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 43);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Year Publish =";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(776, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 43);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Find =";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(93, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 43);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Unit Price =";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(307, 12);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(299, 42);
+            this.txtCode.TabIndex = 7;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(307, 71);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(299, 42);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(307, 127);
+            this.txtQuantity.Multiline = true;
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(299, 42);
+            this.txtQuantity.TabIndex = 9;
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(307, 187);
+            this.txtUnitPrice.Multiline = true;
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(299, 42);
+            this.txtUnitPrice.TabIndex = 10;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.Location = new System.Drawing.Point(989, 12);
+            this.txtAuthor.Multiline = true;
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(299, 42);
+            this.txtAuthor.TabIndex = 11;
+            // 
+            // txtYearPublish
+            // 
+            this.txtYearPublish.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYearPublish.Location = new System.Drawing.Point(989, 71);
+            this.txtYearPublish.Multiline = true;
+            this.txtYearPublish.Name = "txtYearPublish";
+            this.txtYearPublish.Size = new System.Drawing.Size(299, 42);
+            this.txtYearPublish.TabIndex = 12;
+            // 
+            // txtFind
+            // 
+            this.txtFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFind.Location = new System.Drawing.Point(989, 128);
+            this.txtFind.Multiline = true;
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(299, 42);
+            this.txtFind.TabIndex = 13;
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Location = new System.Drawing.Point(99, 249);
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.RowHeadersWidth = 51;
+            this.dgvBook.RowTemplate.Height = 24;
+            this.dgvBook.Size = new System.Drawing.Size(1189, 190);
+            this.dgvBook.TabIndex = 14;
+            this.dgvBook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellContentClick);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(99, 457);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(123, 44);
+            this.btnNew.TabIndex = 15;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(307, 457);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 44);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(748, 457);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(123, 44);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(525, 457);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 44);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(950, 457);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(123, 44);
+            this.btnFind.TabIndex = 19;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(1165, 457);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(123, 44);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 513);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.dgvBook);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.txtYearPublish);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtYearPublish;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace MidtermExam
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-VC57OT1\\SQL2004; Initial Catalog=DBMidterm;" +
+            "Integrated Security=true");
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void getData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From tblBook", con);
+            dt.Clear();
+            da.Fill(dt);
+            dgvBook.DataSource = dt;
+            dgvBook.Columns[0].HeaderText = "Code";
+            dgvBook.Columns[1].HeaderText = "BookTitle";
+            dgvBook.Columns[2].HeaderText = "Quantity";
+            dgvBook.Columns[3].HeaderText = "UnitPrice";
+            dgvBook.Columns[4].HeaderText = "Autor";
+            dgvBook.Columns[5].HeaderText = "YearPublish";
+            dgvBook.Columns["Code"].Width = 140;
+            dgvBook.Columns["Title"].Width = 140;
+            dgvBook.Columns["Quantity"].Width = 140;
+            dgvBook.Columns["UnitPrice"].Width = 140;
+            dgvBook.Columns["Author"].Width = 140;
+            dgvBook.Columns["YearPublish"].Width = 139;
+            dgvBook.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvBook.ColumnHeadersDefaultCellStyle.Font = new Font("Tahama", 14, FontStyle.Bold);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            getData();
+        }
+
+        private void dgvBook_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int Index = e.RowIndex;
+                DataGridViewRow SelectRow = dgvBook.Rows[Index];
+                txtCode.Text = SelectRow.Cells["Code"].Value.ToString();
+                txtTitle.Text = SelectRow.Cells["Title"].Value.ToString();
+                txtQuantity.Text = SelectRow.Cells["Quantity"].Value.ToString();
+                txtUnitPrice.Text = SelectRow.Cells["UnitPrice"].Value.ToString();
+                txtAuthor.Text = SelectRow.Cells["Author"].Value.ToString();
+                txtYearPublish.Text = SelectRow.Cells["YearPublish"].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //no need Code colums because it's auto ingremant
+                SqlCommand cmd = new SqlCommand(" INSERT INTO tblBook(Title,Quantity,UnitPrice,Author,YearPublish)VALUES('" + txtTitle.Text + "','" + int.Parse(txtQuantity.Text) + "','" + Double.Parse(txtUnitPrice.Text) + "','" + txtAuthor.Text + "','" + int.Parse(txtYearPublish.Text) + "'); ", con);
+                cmd.ExecuteNonQuery();
+                getData();
+                btnSave.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult iDelete = DialogResult.OK;
+            iDelete = MessageBox.Show("Confirm if you want to delete this record", "Delete Record...!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (iDelete == DialogResult.Yes)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("Delete from tblBook where Code= '" + int.Parse(txtCode.Text) + "' ", con);
+                    cmd.ExecuteNonQuery();
+                    getData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Update tblBook Set " + " Title = ' " + txtTitle.Text + "', " + " Quantity = ' " + int.Parse(txtQuantity.Text) + "'," +
+            " UnitPrice ='  " + double.Parse(txtUnitPrice.Text) + "', " +
+            " Author ='  " + txtAuthor.Text + "', " +
+             " YearPublish ='  " + int.Parse(txtYearPublish.Text) + "' " +
+            " Where Code =  ' " + int.Parse(txtCode.Text) + "' ", con);
+
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Update data successfull...!", "Update Data...!");
+                getData();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Update data unsuccessfull...!", "Update Data...!");
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * From tblBook" + " Where Title= '" + txtFind.Text + "' ", con);
+            dt.Clear();
+            da.Fill(dt);
+            dgvBook.DataSource = dt;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit;
+            iExit = MessageBox.Show("Confirm if you want to exit...!", "Exit Programm",
+                                     MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (iExit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+                btnSave.Enabled = true;
+                txtTitle.Clear();
+                txtQuantity.Clear();
+                txtUnitPrice.Clear();
+                txtAuthor.Clear();
+                txtYearPublish.Clear();
+                txtCode.Text ="auto number";
+                txtFind.Clear();
+
+        }
+    }
+}
